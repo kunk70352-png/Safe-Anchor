fe5797fc357123037865732ccf82f3eb00acd6ae
kunk70352-png <kunk70352@gmail.com>
2026-07-04 14:32:06 +0800

    Revert "修复拾取地图锚点后无法投掷"
    This reverts commit 27175630a12bdd7ce488d6f32d16c551df5eacb4.

--- src/player/player.gd
+++ src/player/player.gd
@@ -82,7 +82,6 @@ func _check_pickup() -> void:
 	for anchor in anchors.get_children():
 		if global_position.distance_to(anchor.global_position) < pickup_dist:
 			anchor.pick_up()
-			has_anchor = true
 			break
 
 
